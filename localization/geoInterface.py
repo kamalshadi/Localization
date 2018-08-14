f8c0d8454d662693afaa666a089bfc7549348138
Sandesh <vimmrana0@gmail.com>
2018-08-14 19:21:26 +0545

    Change python2 to python 3
    	- Fix Raise error and exception
    	- Fix print statement

--- localization/geoInterface.py
+++ localization/geoInterface.py
@@ -2,25 +2,25 @@
 
 
 class Anchor:
-	def __init__(self,ID,loc):
-		self.loc=loc
-		self.ID=str(ID)
+    def __init__(self, ID, loc):
+        self.loc = loc
+        self.ID = str(ID)
 
-	def __str__(self):
-		return 'Anchor '+self.ID+' @ '+self.loc.__str__()
+    def __str__(self):
+        return 'Anchor ' + self.ID + ' @ ' + self.loc.__str__()
 
 
 class Target:
-	def __init__(self,ID):
-		self.loc=None
-		self.ID=str(ID)
-		self.measures=[]
-	def __str__(self):
-		if self.loc is None:
-			return 'Target '+self.ID
-		else:
-			return 'Target '+self.ID+' @ Real Location:'+self.loc.__str__()
+    def __init__(self, ID):
+        self.loc = None
+        self.ID = str(ID)
+        self.measures = []
 
-	def add_measure(self,a,d):
-		self.measures.append((a,d))
+    def __str__(self):
+        if self.loc is None:
+            return 'Target ' + self.ID
+        else:
+            return 'Target ' + self.ID + ' @ Real Location:' + self.loc.__str__()
 
+    def add_measure(self, a, d):
+        self.measures.append((a, d))
